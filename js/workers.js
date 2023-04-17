bb77f268aee0f2765e20d864f0ba2622819161fc
Serdar Gurbanmyradov <sgurbanmyradov@i-neti.ru>
2023-04-17 05:20:53 +0300

    Updates

--- js/workers.js
+++ js/workers.js
@@ -1,7 +1,7 @@
 this.addEventListener('message', async event => {
-  const imageURL = event.data;
+  const { basePath, imageURL } = event.data;
 
-  const response = await fetch(`/${imageURL}`);
+  const response = await fetch(`${basePath}${imageURL}`);
   const blob = await response.blob();
 
   // Send the image data to the UI thread!
