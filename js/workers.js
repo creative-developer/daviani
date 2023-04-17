0e6e159d64759b5517c480a719887b56bb4beeb8
Serdar Gurbanmyradov <sgurbanmyradov@i-neti.ru>
2023-04-17 05:56:03 +0300

    Updates

--- js/workers.js
+++ js/workers.js
@@ -1,16 +1,12 @@
 this.addEventListener('message', async event => {
   const imagesPaths = event.data;
-
   const imagesPromises = imagesPaths.map(async item => {
     const response = await fetch(`${item.basePath}${item.imageURL}`);
     const blob = await response.blob();
     return { blob, imageURL: item.imageURL };
   });
-  // const response = await fetch(`${basePath}${imageURL}`);
+
   const images = (await Promise.allSettled(imagesPromises)).map(item => item.value);
 
-  // const blob = await response.blob();
-
-  // Send the image data to the UI thread!
   self.postMessage(images);
 });
