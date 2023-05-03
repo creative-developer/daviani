192b7d4270594c378593676172461abbc6163d5d
Serdar Gurbanmyradov <sgurbanmyradov@i-neti.ru>
2023-05-03 16:25:38 +0300

    fixes

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,5 @@
+import lax from 'lax.js';
+
 import { smoothScroll } from './modules/smoothScroll.js';
 import { mediaQueriesInit } from './modules/mediaQueries.js';
 import { popupsInit } from './modules/popups.js';
@@ -69,6 +71,77 @@ $(document).ready(() => {
   initLazyLoadImages();
   initForm();
   initFixHeader();
+
+  // Add animation bindings to elements
+  // MQ(
+  //   breakpoints.xl.maxWidth,
+  //   () => {
+  //     lax.init();
+
+  //     // Add a driver that we use to control our animations
+  //     lax.addDriver('scrollY', function () {
+  //       return window.scrollY;
+  //     });
+
+  //     lax.addElements('.advantages__title', {
+  //       scrollY: {
+  //         translateY: [
+  //           ['elInY', 'elCenterY', 'elOutY'],
+  //           {
+  //             [breakpoints.xl.maxWidthNumberValue]: [200, 'screenHeight/3', 'screenHeight'],
+  //           },
+  //         ],
+  //       },
+  //     });
+
+  //     lax.addElements('.advantages__col--left', {
+  //       scrollY: {
+  //         translateY: [
+  //           ['elInY', 'elCenterY', 'elOutY'],
+  //           {
+  //             [breakpoints.xl.maxWidthNumberValue]: ['screenHeight/2', -400, -800],
+  //           },
+  //         ],
+  //       },
+  //     });
+
+  //     lax.addElements('.advantages__col--center', {
+  //       scrollY: {
+  //         translateY: [
+  //           ['elInY', 'elCenterY', 'elOutY'],
+  //           {
+  //             [breakpoints.xl.maxWidthNumberValue]: ['screenHeight/2', -500, -1200],
+  //           },
+  //         ],
+  //       },
+  //     });
+
+  //     lax.addElements('.advantages__col--right', {
+  //       scrollY: {
+  //         translateY: [
+  //           ['elInY', 'elCenterY', 'elOutY'],
+  //           {
+  //             [breakpoints.xl.maxWidthNumberValue]: ['screenHeight/2', -450, -1000],
+  //           },
+  //         ],
+  //       },
+  //     });
+
+  //     //
+  //   },
+  //   () => {},
+  // );
+
+  // const settings = [
+  //   { element: '.advantages__title-wrap', scale: 1.1, overflow: true, transition: 'cubic-bezier(0,0,1,1)' },
+  //   { element: '.advantages__col--left .advantages__inner', scale: 1.2, overflow: true, transition: 'cubic-bezier(0,0,1,1)' },
+  //   { element: '.advantages__col--center .advantages__inner', scale: 1.5, overflow: true, transition: 'cubic-bezier(0,0,1,1)' },
+  //   { element: '.advantages__col--right .advantages__inner', scale: 1.4, overflow: true, transition: 'cubic-bezier(0,0,1,1)' },
+  // ];
+
+  // settings.forEach(({ element, ...settings }) => {
+  //   new simpleParallax(document.querySelector(element), settings);
+  // });
 
   $(window).on('load', () => {
     gsapMatchMedia.add(breakpoints.xl.minWidth, () => {
